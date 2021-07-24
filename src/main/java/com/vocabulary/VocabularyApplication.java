787e7614e24fc10bee4d1bb6d45586d505b3dd29
Dmitrii Shchepachkov <dmitrii.shchepachkov@netcracker.com>
2021-07-24 14:25:39 +0300

    init

---
+++ src/main/java/com/vocabulary/VocabularyApplication.java
@@ -0,0 +1,12 @@
+package com.vocabulary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VocabularyApplication.class, args);
+    }
+}
